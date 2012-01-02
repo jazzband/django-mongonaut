5e02316ecd277ff1df58efc9e71bbeaafc973f90
pydanny <pydanny@gmail.com>
2012-01-02 09:11:52 -0800

    Basic urls

---
+++ mongonaut/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls.defaults import patterns, url
+
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+
+
+urlpatterns = patterns('',
+)
