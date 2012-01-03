2ad49e3e5f3fb340b60dddad5ddd302f7d53130d
pydanny <pydanny@gmail.com>
2012-01-03 08:18:08 -0800

    Working on the sites display

--- mongonaut/views.py
+++ mongonaut/views.py
@@ -1,18 +1,21 @@
 from django.views.generic import DetailView
 from django.views.generic import ListView
 
+class IndexView(ListView):
+    pass
+
 class AppListView(ListView):
-    """ :args: <app_name> """
+    """ :args: <app_label> """
     pass
 
 class DocumentListView(ListView):
-    """ :args: <app_name> <document_name> """
+    """ :args: <app_label> <document_name> """
     pass
     
 class DocumentDetailView(DetailView):
-    """ :args: <app_name> <document_name> <id> """
+    """ :args: <app_label> <document_name> <id> """
     pass
 
 class DocumentEmbeddedDetailView(DetailView):
-    """ :args: <app_name> <document_name> <id> <???> """
+    """ :args: <app_label> <document_name> <id> <???> """
     pass
