5531183f8c745b302b88816800fbe094a9a2287c
pydanny <pydanny@gmail.com>
2012-01-05 12:22:08 -0800

    A working, simple document list view

--- mongonaut/urls.py
+++ mongonaut/urls.py
@@ -3,8 +3,14 @@ from django.conf.urls.defaults import patterns, url
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView
 
+from mongonaut import views
 
 urlpatterns = patterns('',
+    url(
+        regex=r'^2/(?P<app_label>[_\-\w]+)/(?P<document_name>[_\-\w]+)/$',
+        view=views.DocumentListView.as_view(),
+        name="document_list"
+    )
 )
 
 
--- mongonaut/views.py
+++ mongonaut/views.py
@@ -18,22 +18,28 @@ class DocumentListView(ListView):
     """ :args: <app_label> <document_name> """
     template_name = "mongonaut/document_list.html"
     queryset = []
-    
+
     def get_queryset(self):
-        queryset = super(DocumentListView, self).get_queryset(**kwargs):
         app_label = self.kwargs.get('app_label')
         document_name = self.kwargs.get('document_name')
-        
+
         # TODO Allow this to be assigned via url variable
-        models_name = self.kwargs.get('models_name', 'models.py')
-        
+        models_name = self.kwargs.get('models_name', 'models')
+
         # import the models file
-        model_name = "{0}.{1}".format(document_name, models_name)
+        model_name = "{0}.{1}".format(app_label, models_name)
+        print model_name
         models = importlib.import_module(model_name)
-        
+
         # now get the document
         document = getattr(models, document_name)
         return document.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(DocumentListView, self).get_context_data(**kwargs)
+        context['document_name'] = self.kwargs.get('document_name')
+        return context
+
     
 class DocumentDetailView(DetailView):
     """ :args: <app_label> <document_name> <id> """
