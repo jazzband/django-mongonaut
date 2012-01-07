a780b307754d43ceaa76880be0ea703630846d9a
pydanny <pydanny@gmail.com>
2012-01-07 08:28:09 -0800

    Simple blog

--- examples/blog/entries/models.py
+++ examples/blog/entries/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+from mongoengine import Document, StringField, ReferenceField, ListField
+from mongoengine import EmbeddedDocumentField, EmbeddedDocument
 
-# Create your models here.
+class User(Document):
+    email = StringField(required=True)
+    first_name = StringField(max_length=50)
+    last_name = StringField(max_length=50)
+
+class Post(Document):
+    title = StringField(max_length=120, required=True)
+    author = ReferenceField(User)
+    tags = ListField(StringField(max_length=30))
+    comments = ListField(EmbeddedDocumentField(Comment))    
+
+class TextPost(Post):
+    content = StringField()
+
+class ImagePost(Post):
+    image_path = StringField()
+
+class LinkPost(Post):
+    link_url = StringField()
+    
+    
+class Comment(EmbeddedDocument):
+    content = StringField()
+    name = StringField(max_length=120)
--- examples/blog/settings.py
+++ examples/blog/settings.py
@@ -115,17 +115,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -143,3 +134,6 @@ LOGGING = {
         },
     }
 }
+
+from mongoengine import *
+connect('tumblelog')
