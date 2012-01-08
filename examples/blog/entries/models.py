bd8fbf18d27623dfb1aec184bf966643bc88e8ba
pydanny <pydanny@gmail.com>
2012-01-08 10:09:04 -0800

    simplification of blog

--- examples/blog/entries/models.py
+++ examples/blog/entries/models.py
@@ -9,19 +9,10 @@ class User(Document):
 class Post(Document):
     title = StringField(max_length=120, required=True)
     author = ReferenceField(User)
+    content = StringField()    
     tags = ListField(StringField(max_length=30))
     comments = ListField(EmbeddedDocumentField(Comment))    
 
-class TextPost(Post):
-    content = StringField()
-
-class ImagePost(Post):
-    image_path = StringField()
-
-class LinkPost(Post):
-    link_url = StringField()
-    
-    
 class Comment(EmbeddedDocument):
     content = StringField()
     name = StringField(max_length=120)
