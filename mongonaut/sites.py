64ac05b7c34a79468ac7b58d5903d04d7ec819f8
pydanny <pydanny@gmail.com>
2012-01-10 05:04:26 -0800

    moar work on the sites

--- mongonaut/sites.py
+++ mongonaut/sites.py
@@ -1,3 +1,48 @@
-class MongoAdmin(object):
+from django.core.paginator import Paginat
+
+try:
+    import floppyforms as forms
+except ImportError:
+    from django import forms
     
-    pass
+# TODO add default widgets    
+
+class BaseMongoAdmin(object):
+    
+    search_fields = []
+    
+    #This shows up on the DocumentListView of the Posts
+    list_actions = [] 
+    
+    # This shows up in the DocumentDetailView of the Posts.
+    document_actions = []
+    
+    # shows up on a particular field
+    field_actions = {}
+    
+    fields = None
+    exclude = None
+    fieldsets = None
+    form = forms.ModelForm
+    filter_vertical = ()
+    filter_horizontal = ()
+    radio_fields = {}
+    prepopulated_fields = {}
+    formfield_overrides = {}
+    readonly_fields = ()
+    ordering = None
+
+class MongoAdmin(BaseMongoAdmin):
+    
+    list_display = ('__str__',)
+    list_display_links = ()
+    list_filter = ()
+    list_select_related = False
+    list_per_page = 100
+    list_max_show_all = 200
+    list_editable = ()
+    search_fields = ()
+    save_as = False
+    save_on_top = False
+    paginator = Paginator
+    inlines = []
