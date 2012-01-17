7c8ad46c8636b2ea8783861c655e4db222df7382
pydanny <pydanny@gmail.com>
2012-01-16 19:24:32 -0800

    Start form POST handler

--- mongonaut/forms.py
+++ mongonaut/forms.py
@@ -8,7 +8,8 @@ class DocumentListForm(forms.Form):
     mongo_id = forms.MultipleChoiceField(required=True, widget=forms.CheckboxSelectMultiple)
 
 def document_detail_form_initial(form, document_type, document):
-    """ Adds document field to a form. Not sure what to call this but Factory is not it."""
+    """ Adds document field to a form. Not sure what to call this but Factory is not it.
+    Handle during GET """    
     for key in sorted([x for x in document_type._fields.keys() if x != 'id']):
         if isinstance(document_type._fields[key], EmbeddedDocumentField):            
             continue
@@ -16,6 +17,19 @@ def document_detail_form_initial(form, document_type, document):
             continue
         form.fields[key] = forms.CharField(key, initial=getattr(document, key), widget=get_widget('TODO - assign value'))
     return form
+
+def document_detail_form_process(form, document_type, document, request):
+    """ Handle during POST """
+    for key in sorted([x for x in document_type._fields.keys() if x != 'id']):
+        if isinstance(document_type._fields[key], EmbeddedDocumentField):            
+            continue
+        if isinstance(document_type._fields[key], ListField):                                
+            continue
+        form.fields[key] = forms.CharField(key, widget=get_widget('TODO - assign value'))
+        
+        
+    
+
 
 
 class DocumentDetailForm(forms.Form):
