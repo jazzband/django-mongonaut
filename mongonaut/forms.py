8a6650d40458230977d436fe6ebee396b5a50f82
pydanny <pydanny@gmail.com>
2012-01-16 19:04:35 -0800

    Nixing icky FormFactory that wouldn't work anyway. So there

--- mongonaut/forms.py
+++ mongonaut/forms.py
@@ -3,33 +3,9 @@ from django import forms
 from mongoengine.fields import EmbeddedDocumentField, ListField
 from mongonaut.widgets import get_widget
 
-
-
 class DocumentListForm(forms.Form):
     """ The main document list form """
     mongo_id = forms.MultipleChoiceField(required=True, widget=forms.CheckboxSelectMultiple)
-    
-    
-class DocumentDetailFormFactory(object):
-    """ Used to generate DocumentDetailForms for the DocumentDetailView
-        TODO: Prolly nix
-    """
-    
-    def __init__(self, document, document_type):
-        
-        self.document_type
-        self.document = document
-        self.form = DocumentDetailForm()
-        
-        for key in sorted([x for x in self.document_type._fields.keys() if x != 'id']):
-            # TODO - skip EmbeddedDocumentField and ListField for now
-            if isinstance(self.document._fields[key], EmbeddedDocumentField):            
-                continue
-            if isinstance(self.document._fields[key], ListField):                                
-                continue
-            self.form.fields[key].append(
-                get_widget('TODO - assign value')
-            )
 
 def document_detail_form_initial(form, document_type, document):
     """ Adds document field to a form. Not sure what to call this but Factory is not it."""
