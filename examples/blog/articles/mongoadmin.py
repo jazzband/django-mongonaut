7debc216c0994c99d3fd26bcb960c87f7985191c
pydanny <pydanny@gmail.com>
2012-01-16 18:36:12 -0800

    Breadcrumbs so I don't feel so insane anymore

--- examples/blog/articles/mongoadmin.py
+++ examples/blog/articles/mongoadmin.py
@@ -3,5 +3,4 @@ from mongonaut.sites import MongoAdmin
 from articles.models import Post, User
 
 Post.mongoadmin = MongoAdmin()
-#Post.mongoadmin = True
-User.mongoadmin = True
+User.mongoadmin = MongoAdmin()
