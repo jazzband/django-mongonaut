60d40b1eeadb0fd8e7972c205e0280178d25eaa8
Daniel Greenfeld <pydanny@gmail.com>
2012-01-21 21:56:28 -0800

    Cleanup of unused exception and removing unused file

--- mongonaut/exceptions.py
+++ mongonaut/exceptions.py
@@ -1,7 +1,3 @@
-class MissingPreliminaryMethod(Exception):
-    """ Thrown when a preliminary method is not called. """
-    pass
-
 class NoMongoAdminSpecified(Exception):
     """ Called when no MongoAdmin is specified. Unlike to ever be called."""
     pass
