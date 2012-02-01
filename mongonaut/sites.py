a72ec693c85090215c85bf1ce0bbcd11db8fde2d
Daniel Greenfeld <pydanny@gmail.com>
2012-02-01 13:36:28 -0800

    Fixing permissions again. Why don't I remember this silly call?

--- mongonaut/sites.py
+++ mongonaut/sites.py
@@ -49,7 +49,7 @@ class BaseMongoAdmin(object):
 
     def has_delete_permission(self, request):
         """ Can delete this object """
-        return request.user.is_authenticated and request.user.is_active and request.user.is_admin
+        return request.user.is_authenticated and request.user.is_active and request.user.is_superuser
 
 
 class MongoAdmin(BaseMongoAdmin):
