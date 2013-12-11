1cbae53d16bbd4147cb1aa74611b4197e187f9d3
Jeremy Johnson <jeremy@softworks.com.my>
2013-12-11 16:29:38 +0800

    this should allow tests to run on python 2.6

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
 
-#set path
+try: 
+     import unittest2 as unittest 
+except ImportError: 
+    import unittest 
+
 import sys
-sys.path.append("..")
-sys.path.append("../examples/blog") #we are using settings.py from the example
+import os
+
+#set path
+TEST_ROOT = os.path.dirname(__file__)
+PROJECT_ROOT = os.path.join(TEST_ROOT, '..')
+BLOG_ROOT = os.path.join(PROJECT_ROOT, 'examples','blog')
+
+sys.path.append(PROJECT_ROOT)
+sys.path.append(BLOG_ROOT) #we are using settings.py from the example
 
 #Ensure Django is configured to use our example site
-import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'examples.blog.settings'
 
 
 #run the tests
-import glob
-import unittest
-
-tests = unittest.defaultTestLoader.discover('.',pattern='*_tests.py')
+tests = unittest.defaultTestLoader.discover(TEST_ROOT, pattern='*_tests.py')
 runner = unittest.TextTestRunner(verbosity=2)
 result = runner.run(unittest.TestSuite(tests))
